77f645b7acf5c9a40abf07acc3a4980076715b39
Mar10Josh <65928867+Mar10Josh@users.noreply.github.com>
2021-07-05 13:25:03 -1000

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def on_forever():
-    basic.show_string(custom.find_is_greater_or_less(1, 0))
-    basic.show_arrow(ArrowNames.WEST)
-    basic.show_arrow(ArrowNames.EAST)
-    basic.show_arrow(ArrowNames.WEST)
-    basic.show_arrow(ArrowNames.EAST)
-basic.forever(on_forever)
